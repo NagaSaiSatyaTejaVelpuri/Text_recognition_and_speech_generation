da7ec0b08adc7188f304d094e15c861d15974140
Naga Sai Satya Teja Velpuri <45967918+NagaSaiSatyaTejaVelpuri@users.noreply.github.com>
2020-03-15 12:38:52 +0530

    Delete Final code.py

--- Final code.py
+++
@@ -1,7 +0,0 @@
-import os
-from gtts import gTTS
-
-language='en'
-output=gTTS(text="wake up    wake up    wake up", lang=language,slow=False)
-output.save("output2.mp3")
-os.system("start output2.mp3")
